11146b17ac31847c50a563de48f37edd1982225c
WOOK0112 <124886494+WOOK0112@users.noreply.github.com>
2023-06-01 13:52:31 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
 else
@@ -24,6 +24,6 @@ else
 fi
 
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+DEPLOY_JAR="$DEPLOY_PATH$JAR_NAME"
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+sudo nohup java -jar "$DEPLOY_JAR" >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
